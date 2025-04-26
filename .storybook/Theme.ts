895500f6bea3dd596d7f4db3b154a0496f3e3848
Kallil Belmonte <kallil.belmonte@gmail.com>
2025-04-25 21:18:13 -0300

    Theme updated.

--- .storybook/Theme.ts
+++ .storybook/Theme.ts
@@ -11,4 +11,8 @@ export default create({
   brandUrl: 'https://vuejs.org',
   brandImage: '/logo/vue.svg',
   brandTarget: '_blank',
+
+  // Colors
+  colorPrimary: '#43B883',
+  colorSecondary: '#43B883',
 });
